fe75cf59050d0ef1eb4b0c789f55fa746172db2f
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2024-06-13 23:44:45 -0500

    updates

---
+++ OOP-script.js
@@ -0,0 +1,37 @@
+const tmdbAPI = 'https://api.themoviedb.org/3/movie/popular?api_key=d85fc3f866e5fc77be2f384a028b16d3&append_to_response=images'
+
+
+
+fetch(tmdbAPI)
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+        console.log(data.results)
+
+        const movieArray = data.results
+        displayMovieHomePage(movieArray)
+})
+
+
+function displayMovieHomePage(movieArray){
+    const mainHOMEGridContainer = document.querySelector('.main-Home-grid-container')    
+
+    movieArray.forEach(element => {
+
+        let movieYear = new Date(element.release_date) 
+        const movieYearConverted = movieYear.getFullYear()
+
+        mainHOMEGridContainer.innerHTML = `
+            <div class="movie-grid-container">
+                <div class="img-container">
+                    <img class="imgPoster" src="" alt="" />
+                </div>
+                <div class="info-container">
+                    <p class="movie-title">${element.title}</p>
+                    <p class="movie-year">${movieYearConverted}</p>
+                </div>
+            </div>`
+    });
+
+    console.log(movieArray)
+}
