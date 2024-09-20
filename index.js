9302bb8d3fced5b34494f110129c7b4674abb3fe
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2024-09-19 23:55:36 -0500

    Updted the code to make it shorter but equaly effective.

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const searchAPI = 'https://api.themoviedb.org/3/search/keyword?query=${query}&pa
     const searchInput = document.querySelector('.searchForm')
     
     
-    searchBTN.addEventListener('click', fetchMoviesSearch)
-    
+    searchBTN.addEventListener('click', () => fetchMoviesSearch(displayMovieHomePage))
     
     function fetchMoviesSearch(){
 
@@ -21,175 +20,82 @@ const searchAPI = 'https://api.themoviedb.org/3/search/keyword?query=${query}&pa
 
             alert("Please, write something") 
             
-        }
-            else  {
-                        // const query = document.querySelector('.searchForm')
-                        // const inputValue = query.value;
+        }else{
+                const searchAPI = `https://api.themoviedb.org/3/search/movie?api_key=d85fc3f866e5fc77be2f384a028b16d3&language=en-US&query=${inputValue}&page=1`
                 
-                        const searchAPI = `https://api.themoviedb.org/3/search/movie?api_key=d85fc3f866e5fc77be2f384a028b16d3&language=en-US&query=${inputValue}&page=1`
-                
-                        fetch(searchAPI)
-                        .then(response => response.json())
-                        .then(data => {
-                            const movieList = data.results
-                            const mainHomeGrid = document.querySelector('.main-Home-grid-container')
+                fetch(searchAPI)
+                .then(response => response.json())
+                .then(data => {
+                    const movieDataResults = data.results;
+                    const mainHomeGrid = document.querySelector('.main-Home-grid-container')
 
-                            mainHomeGrid.innerHTML = '';
+                    mainHomeGrid.innerHTML = '';
 
-
-                            movieList.forEach(index => {
-                
-                                const dateConverted = index.release_date;
-                                const year = new Date(dateConverted).getFullYear();
-            
-                                const movieContainer = document.createElement('div')
-                                movieContainer.classList.add('movie-grid-container')
-
-                                movieContainer.innerHTML = `
-                                    <div class="img-container">
-                                        <div class="littleIconContainer">
-                                        <img src="little icon.svg" class="littleIconMenu">
-                                        </div>
-                                        <img class="imgPoster" src="https://image.tmdb.org/t/p/w500${index.poster_path}" alt="" />
-                                        <div class="figcaptionContainer">
-                                        <p class="contentFigcaption">${index.overview}</p>
-                                        </div>
-                                    </div>
-            
-                                    <div class="info-container">
-                                        <p class="movie-title">${index.title}</p>
-                                        <p class="movie-year">${year}</p>
-                                    </div>`
-                
-                            mainHomeGrid.appendChild(movieContainer)
-                
-                            })   
-                        })
-                        .catch(err => console.log(err))
+                    displayMovieHomePage(movieDataResults)
+                })
+                .catch(err => console.log(err))
             }
     }
 
-        
-
+   
 //------------------------------------------------------------//
 
 
-//--------------HOME PAGE SECTION-----------------------------//
-    document.addEventListener('DOMContentLoaded', fetchMovies)
+
+//--------------HOME PAGE SECTION ORIGINAL---------------------------//
+    document.addEventListener('DOMContentLoaded', fetchMovies(displayMovieHomePage))
 
 
     function fetchMovies(){
         
-        
         fetch(apiPopular)
         .then(response => response.json())
         .then(data => {
-            const movieInformation = data.results;
+            const mainHomeGrid = document.querySelector('.main-Home-grid-container')
+            mainHomeGrid.innerHTML = '';
 
-            console.log(data.results)
+            let movieDataResults = data.results;
+            console.log(movieDataResults)
 
-            movieInformation.forEach(index => {
-                
-                const homeGridContainer = document.querySelector('.main-Home-grid-container')
-                const convertDate = index.release_date;
-                movieYear = new Date(convertDate).getFullYear();
-
-                const movieContainer = document.createElement('div')
-                movieContainer.classList.add('movie-grid-container')
-                movieContainer.innerHTML = `
-                        <div class="img-container">
-                            <div class="littleIconContainer">
-                            <img src="little icon.svg" class="littleIconMenu">
-                            </div>
-                            <img class="imgPoster" src="https://image.tmdb.org/t/p/w500${index.poster_path}" alt="" />
-                            <div class="figcaptionContainer">
-                            <p class="contentFigcaption">${index.overview}</p>
-                            </div>
-                        </div>
-
-                        <div class="info-container">
-                            <p class="movie-title">${index.title}</p>
-                            <p class="movie-year">${movieYear}</p>
-                        </div>`
-
-                homeGridContainer.appendChild(movieContainer)
-
-            })
+            displayMovieHomePage(movieDataResults)
         })
     }
+
 //------------------------------------------------------------//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* -------------------------------------------- ORIGINAL ------------------------------------------------------------ */
-// const addMovieBtn = document.querySelector('.addMovieBTN')
-// addMovieBtn.addEventListener('click', searchMovies)
-
-
-
-// function searchMovies(){
-
-//     const searchMovie = document.querySelector('#searchInput').value;
-
-//     const apiURLSearch = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${searchMovie}&page=1`;
-
-//     fetch(apiURLSearch)
-//     .then(res => res.json())
-//     .then(data => {
-//         console.log(data)
-//         console.log(data.results)
-//         const movie = data.results;
-
-//         movie.forEach(element => {
     
-//             const masterMovieContainer = document.querySelector('.containerMoviesInsideListDashboard')
+   
+function displayMovieHomePage(movieDataResults){
+
+    movieDataResults.forEach(index => {
+        
+        const homeGridContainer = document.querySelector('.main-Home-grid-container')
+                   
+        const convertDate = index.release_date;
+        movieYear = new Date(convertDate).getFullYear();
     
-//             const movieContainer = document.createElement('div')
-//             const titleContainerMovie = document.createElement('div')
-//             movieContainer.innerHTML = `<img class="containerThumbnailMovieDashboard" src="https://image.tmdb.org/t/p/w500${element.poster_path}">
-
-//                     <p>${element.release_date}</p>`
+        const movieContainer = document.createElement('div')
+        movieContainer.classList.add('movie-grid-container')
+        movieContainer.innerHTML = `
+                <div class="img-container">
+                    <div class="littleIconContainer">
+                    <img src="little icon.svg" class="littleIconMenu">
+                    </div>
+                    <img class="imgPoster" src="https://image.tmdb.org/t/p/w500${index.poster_path}" alt="" />
+                    <div class="figcaptionContainer">
+                    <p class="contentFigcaption">${index.overview}</p>
+                    </div>
+                </div>
     
-//             movieContainer.classList.add('containerMovieDashboard')
+                <div class="info-container">
+                    <p class="movie-title">${index.title}</p>
+                    <p class="movie-year">${movieYear}</p>
+                </div>`
     
-//             titleContainerMovie.innerHTML = `<p class="titleMovieDashboard">${element.title}</p>`
-//             titleContainerMovie.classList.add('containerTitleMovieDashboard')
+        homeGridContainer.appendChild(movieContainer)
+    
+    })
 
-//             movieContainer.appendChild(titleContainerMovie)
-//             masterMovieContainer.appendChild(movieContainer)
-
-//         });
-//     })
-
-//         fetch(apiGenreURL)
-//         .then(res => res.json())
-//         .then(data => {
-//             console.log(data)
-//             // console.log(data.genres)
-//             // console.log(data.genres[0].name)
-//             // console.log(data.genres[1].name)
-//             // console.log(data.genres[2].name)
-            
-//         })
-
-// }
-
+}
 
 
 
