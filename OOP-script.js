75fbe9c54e8f5710a0daa7fc0e43263c0eadc4b3
Renilto Silva <39367292+ReniltoSilva@users.noreply.github.com>
2024-10-07 00:24:05 -0500

    updates

--- OOP-script.js
+++ OOP-script.js
@@ -8,7 +8,7 @@ let movieCollection = {
         .then(data => {
             console.log(data)
             const movieArray = data.results
-            displayMovieHomePage(movieArray)
+            this.displayMovieHomePage(movieArray)
         })
     },
 
